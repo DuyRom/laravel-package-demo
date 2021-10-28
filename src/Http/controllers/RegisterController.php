ee7ca0a03aab05002905e4a57bccebbc3d543d45
duynguyen <duyit192@hotmail.com>
2021-10-28 20:35:28 +0700

    larave package demo

---
+++ src/Http/controllers/RegisterController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace duynguyen\CustomAuth\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Routing\Controller;
+
+class RegisterController extends Controller
+{
+    public function create()
+    {
+        return view('custom-auth::register');
+    }
+
+    public function store(Request $request)
+    {
+        dd($request);
+    }
+}
